4d35c5428044b5da068962c651a5ced8889e7747
vochilong <vochilong.work@gmail.com>
2024-03-09 23:05:20 +0700

    update

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-console.log("Hello, World!");
+const { Client } = require('ssh2');
+
+async function main() {
+    const conn = new Client();
+    await new Promise((resolve, reject) => {
+        conn.on('ready', () => {
+            console.log('SSH connection established');
+            conn.exec('ls', (err, stream) => {
+                if (err) throw err;
+                stream.on('close', (code, signal) => {
+                    console.log('Stream closed');
+                    conn.end();
+                    resolve();
+                }).on('data', (data) => {
+                    console.log('STDOUT:', data.toString());
+                }).stderr.on('data', (data) => {
+                    console.error('STDERR:', data.toString());
+                });
+            });
+        }).on('error', (err) => {
+            console.error('SSH connection error:', err);
+            reject(err);
+        }).connect({
+            host: process.env.HOST,
+            port: process.env.PORT || 22,
+            username: process.env.USERNAME,
+            password: process.env.PASSWORD
+        });
+    });
+}
+
+main().catch((err) => {
+    console.error(err);
+    process.exit(1);
+});
